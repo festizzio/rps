48ab38c7aa84fb5082aa7e76a9c95d358fed3389
Sean <festizzio@gmail.com>
2022-09-29 07:21:44 -0700

    Add function for comparing computer and player choice

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
 function getComputerChoice() {
-    // Return random result from Rock, Paper, or Scissors.
-    // Will be used to calculate computer's choice
+    let rock = "rock", paper = "paper", scissors = "scissors";
+    let choice = Math.floor((Math.random() * 3) + 1);
+    switch(choice) {
+        case 1: 
+            return rock;
+            break;
+        case 2:
+            return paper;
+            break;
+        case 3: 
+            return scissors;
+            break;
+        default:
+            return "No selection";
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
     /* Compare lowercase of playerSelection vs. computer Selection
     and return the winner. Rock > Scissors, Scissors > Paper, and
     Paper > Rock. */
+
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+
+    if(player === computer) {
+        return 0;
+    } else if((player === "rock" && computer === "scissors") ||
+              (player === "scissors" && computer === "paper") ||
+              (player === "paper" && computer === "rock")) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+for(let i = 0; i < 10; i++) {
+    let player = getComputerChoice();
+    let computer = getComputerChoice();
+    let result = playRound(player, computer);
+    let resultString = result > 0 ? "The player!" 
+                    : (result < 0 ? "The computer!" : "Nobody, it's a tie!");
+    console.log(`Player chose ${player} and computer chose ${computer}.
+                The winner is ${resultString}`);
 }
 
 function game() {
