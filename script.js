b2d8134288c83821be360e2fb2c2e3f42acef0e3
Sean <festizzio@gmail.com>
2022-09-29 06:57:17 -0700

    Add skeleton for Javascript functions

---
+++ script.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+    // Return random result from Rock, Paper, or Scissors.
+    // Will be used to calculate computer's choice
+}
+
+function playRound(playerSelection, computerSelection) {
+    /* Compare lowercase of playerSelection vs. computer Selection
+    and return the winner. Rock > Scissors, Scissors > Paper, and
+    Paper > Rock. */
+}
+
+function game() {
+    /* Play 5 rounds of RPS with the computer
+    Each time, prompt the user for a selection, then
+    call the playRound function passing the result returned from
+    getComputerChoice. */
+}
